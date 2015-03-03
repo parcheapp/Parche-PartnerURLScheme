bbdfe4699c2071dc19e6bb1ff5552ed56caeb3c9
Ellen Shapiro <designatednerd@gmail.com>
2015-03-03 14:03:53 -0600

    Initial URL scheme helper.

--- iOS/PartnerURLSchemeSample/PartnerURLSchemeHelper/PARPartnerURLSchemeHelper.h
+++ iOS/PartnerURLSchemeSample/PartnerURLSchemeHelper/PARPartnerURLSchemeHelper.h
@@ -6,8 +6,42 @@
 //  Copyright (c) 2015 Parche. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
+/**
+ *  Helper class to assist in calling Parche's URL scheme.
+ */
 @interface PARPartnerURLSchemeHelper : NSObject
+
+/**
+ *  Determines if there is a version of Parche on the user's device which responds to 
+ *  this URL scheme.
+ *
+ *  @return YES if the application needs to be updated or installed, NO if you are clear to
+ *          go ahead and call the request discount method.
+ */
++ (BOOL)parcheNeedsToBeUpdatedOrInstalled;
+
+/**
+ *  Opens the App store to show the Parche application. If the user does not have the app
+ *  installed, they will see "Get".
+ *
+ *  If the user has Parche installed, they will see "Open" if there is no pending update or
+ *  "Update" if there is a pending update.
+ */
++ (void)showParcheInAppStore;
+
+/**
+ *  <#Description#>
+ *
+ *  @param partnerUserID <#partnerUserID description#>
+ *  @param discountCode  <#discountCode description#>
+ *  @param apiKey        <#apiKey description#>
+ *
+ *  @return YES if the request opened the Parche application successfully, NO if it did not.
+ */
++ (BOOL)requestDiscountForUser:(NSString *)partnerUserID
+                      withCode:(NSString *)discountCode
+                        apiKey:(NSString *)apiKey;
 
 @end
--- iOS/PartnerURLSchemeSample/PartnerURLSchemeHelper/PARPartnerURLSchemeHelper.m
+++ iOS/PartnerURLSchemeSample/PartnerURLSchemeHelper/PARPartnerURLSchemeHelper.m
@@ -8,6 +8,53 @@
 
 #import "PARPartnerURLSchemeHelper.h"
 
+static NSString * const ParcheURLScheme = @"goparche://";
+static NSString * const ParcheOpenEndpoint = @"open";
+static NSString * const ParcheOpenFormat = @"open?partner_user_id=%@&discount_code=%@&api_key=%@";
+static NSString * const ParcheAppStoreURL = @"https://itunes.apple.com/us/app/parche-valet-without-delay/id943516663?ct=partner";
+
 @implementation PARPartnerURLSchemeHelper
+
++ (NSURL *)openWithoutDiscountURL
+{
+    NSString *urlString = [ParcheURLScheme stringByAppendingString:ParcheOpenEndpoint];
+    return [NSURL URLWithString:urlString];
+}
+
++ (BOOL)parcheNeedsToBeUpdatedOrInstalled
+{
+    return ![[UIApplication sharedApplication] canOpenURL:[self openWithoutDiscountURL]];
+}
+
++ (void)showParcheInAppStore
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:ParcheAppStoreURL]];
+}
+
++ (BOOL)openWithoutDiscount
+{
+    if (![self parcheNeedsToBeUpdatedOrInstalled]) {
+        [[UIApplication sharedApplication] openURL:[self openWithoutDiscountURL]];
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
++ (BOOL)requestDiscountForUser:(NSString *)partnerUserID
+                      withCode:(NSString *)discountCode
+                        apiKey:(NSString *)apiKey
+{
+    if (![self parcheNeedsToBeUpdatedOrInstalled]) {
+        
+        NSString *path = [NSString stringWithFormat:ParcheOpenFormat, partnerUserID, discountCode, apiKey];
+        NSString *urlString = [ParcheURLScheme stringByAppendingString:path];
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlString]];        
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
 
 @end
