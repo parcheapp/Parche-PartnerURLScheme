9335a2c0707bbce256361c227e895bcef465efba
Ellen Shapiro <designatednerd@gmail.com>
2015-03-03 14:34:16 -0600

    Update sample app.

--- iOS/PartnerURLSchemeSample/PartnerURLSchemeSample/ViewController.m
+++ iOS/PartnerURLSchemeSample/PartnerURLSchemeSample/ViewController.m
@@ -35,7 +35,38 @@
 
 - (IBAction)showParcheInAppStore
 {
+#if TARGET_IPHONE_SIMULATOR
+    [self showSimpleAlertVCWithTitle:@"Simulator!" message:@"The app store link doesn't work on the simulator. Please test on a device."];
+#else
     [PARPartnerURLSchemeHelper showParcheInAppStore];
+#endif
+}
+
+- (IBAction)openAppWithoutDiscount
+{
+    if (![PARPartnerURLSchemeHelper openParcheWithAPIKey:@"YOUR_API_KEY"]) {
+        //NOTE: Use NSLocalizedString in anything that actually faces your users.
+        [self showSimpleAlertVCWithTitle:@"Ruh Roh!" message:@"You do not have a version of Parche which supports the URL scheme installed."];
+    }
+}
+
+- (void)showSimpleAlertVCWithTitle:(NSString *)title message:(NSString *)message
+{
+    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:title
+                                                                     message:message
+                                                              preferredStyle:UIAlertControllerStyleAlert];
+    
+    typeof(self) __weak weakSelf = self;
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK"
+                                                 style:UIAlertActionStyleDefault
+                                               handler:^(UIAlertAction *action) {
+                                                   [weakSelf dismissViewControllerAnimated:YES completion:nil];
+                                               }];
+    
+    [alertVC addAction:ok];
+    [self presentViewController:alertVC
+                       animated:YES
+                     completion:nil];
 }
 
 @end
